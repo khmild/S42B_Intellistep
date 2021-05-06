6c6a0fa6497f9e9a2422af612c530c9822338396
Christian Piper <42127153+CAP1Sup@users.noreply.github.com>
2021-05-06 17:30:18 -0400

    Fixed version

--- src/user/config.h
+++ src/user/config.h
@@ -5,7 +5,7 @@
 #include "Arduino.h"
 
 // Version of the firmware (displayed on OLED) (follows semantic versioning)
-#define VERSION "0.0.4"
+#define VERSION "0.0.5"
 
 
 // --------------  Settings  --------------
