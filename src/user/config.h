e73382b7c20e478902fc8a4b933e4de4d832b9b3
cap1sup <xmanpiper@gmail.com>
2021-05-07 20:52:34 -0400

    Fixed version

--- src/user/config.h
+++ src/user/config.h
@@ -5,7 +5,7 @@
 #include "Arduino.h"
 
 // Version of the firmware (displayed on OLED) (follows semantic versioning)
-#define VERSION "0.0.5"
+#define VERSION "0.0.6"
 
 
 // --------------  Settings  --------------
