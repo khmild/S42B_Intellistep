7f3560a1e2a07803e750320a66ecac299f22609d
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2021-08-11 13:22:58 +0300

    Invert singn of error according to BT

--- src/hardware/timers.cpp
+++ src/hardware/timers.cpp
@@ -362,21 +362,21 @@ void correctMotor() {
                 if (/*motor.getStepPhase() != */ true) {
                     if (stepDeviation > 0) {
 
-                        // Motor is at a position larger than the desired one
-                        // Use the current angle to find the current step, then subtract 1
-                        #ifdef USE_HARDWARE_STEP_CNT
-                            motor.step(NEGATIVE, false);
-                        #else
-                            motor.step(NEGATIVE, false, false);
-                        #endif
-                    }
-                    else {
                         // Motor is at a position smaller than the desired one
                         // Use the current angle to find the current step, then add 1
                         #ifdef USE_HARDWARE_STEP_CNT
                             motor.step(POSITIVE, false);
                         #else
                             motor.step(POSITIVE, false, false);
+                        #endif
+                    }
+                    else {
+                        // Motor is at a position larger than the desired one
+                        // Use the current angle to find the current step, then subtract 1
+                        #ifdef USE_HARDWARE_STEP_CNT
+                            motor.step(NEGATIVE, false);
+                        #else
+                            motor.step(NEGATIVE, false, false);
                         #endif
                     }
                 }
